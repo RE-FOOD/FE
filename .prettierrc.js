c5c693f799867b3faf4008c284dab86865aba194
최이서 <chtoqur@gmail.com>
2025-08-04 17:53:42 +0900

    chore: 프리티어 포맷 설정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
-  bracketSameLine: true,
+  bracketSameLine: false,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  endOfLine: 'auto',
+  tabWidth: 2,
+  printWidth: 100,
 };

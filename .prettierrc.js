ca09a58306f44f96577d7e0b0dfe43a4076a4159
최이서 <chtoqur@gmail.com>
2025-08-05 11:48:44 +0900

    chore: eslint, prettier 설정 추가(#1)

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: false,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-  endOfLine: 'auto',
+  trailingComma: 'es5',
   tabWidth: 2,
+  semi: true,
+  singleQuote: true,
   printWidth: 100,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  endOfLine: 'auto',
 };

ee27c413cfdf511d9114c3baf568c1711ba38a30
HeHelee <selly3109@naver.com>
2025-08-11 01:33:44 +0900

    feat: 경로 추가 (#15)

--- src/navigations/stack/UserStackNavigator.tsx
+++ src/navigations/stack/UserStackNavigator.tsx
@@ -7,6 +7,8 @@ import UserBottomTabsNavigator, {
 import { userNavigations } from '@/constants/navigations';
 import HistoryDetailScreen from '@/screens/history/HistoryDetailScreen';
 import ReviewWriteScreen from '@/screens/history/ReviewWriteScreen';
+import CouponBox from '@/screens/mypage/CouponBox';
+import NicknameChangeScreen from '@/screens/mypage/NicknameChangeScreen';
 
 export type UserStackParamList = {
   UserTabs: NavigatorScreenParams<UserBottomTabsParamList>;
@@ -15,6 +17,8 @@ export type UserStackParamList = {
     orderId: number;
   };
   [userNavigations.REVIEW_WRITE]: undefined;
+  [userNavigations.NiCKNAME_CHANGE]: undefined;
+  [userNavigations.COUPON_BOX]: undefined;
 };
 
 const Stack = createStackNavigator<UserStackParamList>();
@@ -40,6 +44,16 @@ function UserStackNavigator() {
         name="ReviewWrite"
         component={ReviewWriteScreen}
         options={{ title: '리뷰 작성', headerShown: true }}
+      />
+      <Stack.Screen
+        name="NicknameChange"
+        component={NicknameChangeScreen}
+        options={{ title: '닉네임 변경', headerShown: true }}
+      />
+      <Stack.Screen
+        name="CouponBox"
+        component={CouponBox}
+        options={{ title: '쿠폰함', headerShown: true }}
       />
     </Stack.Navigator>
   );

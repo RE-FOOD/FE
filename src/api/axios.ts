8d8c3c7287807117ef4b27b6988d7c74b5982c87
최이서 <chtoqur@gmail.com>
2025-08-18 17:07:55 +0900

    feat: baseURL 변경 (#17)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,8 @@
+import Config from 'react-native-config';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.39.128.29:8080/api',
-  // baseURL: 'http://192.168.0.76:8080/api', // 이서 데스크탑
-  // baseURL: 'http://192.168.4.197:8080/api', // KOSA (703, 706 데스크탑)
-  // baseURL: 'http://localhost:8080/api',
+  baseURL: Config.API_BASE_URL,
   withCredentials: true,
 });
 

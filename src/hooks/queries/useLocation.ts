6ce1cbf5f71403258e47a7d3357d2b752998947a
최이서 <chtoqur@gmail.com>
2025-08-21 21:11:27 +0900

    feat: 지역 추가 웹뷰 연결, API 연동 (#16)

--- src/hooks/queries/useLocation.ts
+++ src/hooks/queries/useLocation.ts
@@ -1,21 +1,65 @@
-import { useQuery } from '@tanstack/react-query';
-import { getLocations } from '@/api/location';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { getLocations, addLocation, deleteLocation, setDefaultLocation } from '@/api/location';
+import queryClient from '@/api/queryClient';
 import { queryKeys } from '@/constants/keys';
-import { UseQueryCustomOptions } from '@/types/api';
+import { UseMutationCustomOptions, UseQueryCustomOptions } from '@/types/api';
 import { LocationFull } from '@/types/domain';
 
 function useGetLocations(queryOptions?: UseQueryCustomOptions<LocationFull[]>) {
   return useQuery({
-    queryFn: getLocations,
     queryKey: [queryKeys.LOCATION, queryKeys.GET_LOCATIONS],
+    queryFn: getLocations,
     ...queryOptions,
+  });
+}
+
+function useAddLocation(mutationOptions?: UseMutationCustomOptions<LocationFull>) {
+  return useMutation({
+    mutationFn: async ({ address, roadAddress }: { address: string; roadAddress: string }) => {
+      // 주소 추가, 즉시 기본 주소로 변경
+      const newLocation = await addLocation(address, roadAddress);
+      await setDefaultLocation(newLocation.id);
+      return newLocation;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryKeys.LOCATION] });
+    },
+    ...mutationOptions,
+  });
+}
+
+function useDeleteLocation(mutationOptions?: UseMutationCustomOptions<void>) {
+  return useMutation({
+    mutationFn: (locationId: number) => deleteLocation(locationId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryKeys.LOCATION] });
+    },
+    ...mutationOptions,
+  });
+}
+
+function useSetDefaultLocation(mutationOptions?: UseMutationCustomOptions<LocationFull>) {
+  return useMutation({
+    mutationFn: (locationId: number) => setDefaultLocation(locationId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryKeys.LOCATION] });
+    },
+    ...mutationOptions,
   });
 }
 
 function useLocation() {
   const locationsQuery = useGetLocations();
+  const addLocationMutation = useAddLocation();
+  const deleteLocationMutation = useDeleteLocation();
+  const setDefaultLocationMutation = useSetDefaultLocation();
 
-  return { locationsQuery };
+  return {
+    locationsQuery,
+    addLocationMutation,
+    deleteLocationMutation,
+    setDefaultLocationMutation,
+  };
 }
 
 export default useLocation;

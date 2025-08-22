392422142117432db56819e8bb99cae8bd72921e
최이서 <chtoqur@gmail.com>
2025-08-22 19:54:15 +0900

    feat: AOS 네이버 맵 설정 (#34)

--- src/screens/store/StoreInfoScreen.tsx
+++ src/screens/store/StoreInfoScreen.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import StoreMap from '@/components/store/StoreMap';
 import { colors } from '@/constants/colors';
 import { useGetStoreDetail } from '@/hooks/queries/useStore';
 import { UserStackParamList } from '@/navigations/stack/UserStackNavigator';
@@ -49,7 +50,7 @@ const StoreInfoScreen = () => {
             </View>
           </View>
         </View>
-        <View>{/* 지도 */}</View>
+        <StoreMap />
         <View style={{ gap: 8 }}>
           <Text style={styles.subTitle}>주소</Text>
           <Text style={styles.text}>{store?.address}</Text>

47852e76dc9bb639b9afa6840d864502a06b9796
HeHelee <selly3109@naver.com>
2025-08-30 20:57:32 +0900

    feat: 경로 추가 (#53)

--- src/constants/navigations.ts
+++ src/constants/navigations.ts
@@ -38,6 +38,8 @@ const sellerNavigations = {
   MYPAGE_HOME: 'MypageHome',
   ORDER_HOME: 'OrderHome',
   MENU_HOME: 'MenuHome',
+  MENU_REGISTER: 'MenuRegister',
+  MENU_MODIFY: 'MenuModify',
 };
 
 export { loggedOutNavigations, userNavigations, sellerNavigations };

50366efa3985dd2863ba675b0585b4d2cee60fc2
HeHelee <selly3109@naver.com>
2025-08-30 20:58:30 +0900

    feat: 화면 등록 (#53)

--- src/navigations/stack/SellerStackNavigator.tsx
+++ src/navigations/stack/SellerStackNavigator.tsx
@@ -4,12 +4,16 @@ import SellerBottomTabsNavigator, {
   SellerBottomTabsParamList,
 } from '../bottomTabs/SellerBottomTabsNavigator';
 import { sellerNavigations } from '@/constants/navigations';
+import SellerMenuModifyScreen from '@/screens/seller/SellerMenuModifyScreen';
+import SellerMenuRegisterScreen from '@/screens/seller/SellerMenuRegisterScreen';
 
 export type SellerStackparamList = {
   SellerTabs: NavigatorScreenParams<SellerBottomTabsParamList>;
   [sellerNavigations.MENU_HOME]: undefined;
   [sellerNavigations.ORDER_HOME]: undefined;
   [sellerNavigations.MYPAGE_HOME]: undefined;
+  [sellerNavigations.MENU_REGISTER]: undefined;
+  [sellerNavigations.MENU_MODIFY]: undefined;
 };
 
 const Stack = createStackNavigator<SellerStackparamList>();
@@ -25,6 +29,16 @@ function SellerStackNavigator() {
         name="SellerTabs"
         component={SellerBottomTabsNavigator}
         options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name={sellerNavigations.MENU_REGISTER}
+        component={SellerMenuRegisterScreen}
+        options={{ title: '메뉴 등록' }}
+      />
+      <Stack.Screen
+        name={sellerNavigations.MENU_MODIFY}
+        component={SellerMenuModifyScreen}
+        options={{ title: '메뉴 수정' }}
       />
     </Stack.Navigator>
   );

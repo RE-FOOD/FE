cf9f5a1eb0ce2b9457442f2fb93991f7c0d242c1
최이서 <chtoqur@gmail.com>
2025-08-30 18:51:21 +0900

    feat: 바텀탭 아이콘 설정 및 스타일링 (#44)

--- src/navigations/bottomTabs/UserBottomTabsNavigator.tsx
+++ src/navigations/bottomTabs/UserBottomTabsNavigator.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { SvgProps } from 'react-native-svg';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import FavoriteActive from '@/assets/icons/favorite-active.svg';
+import Favorite from '@/assets/icons/favorite.svg';
+import HomeActive from '@/assets/icons/home-active.svg';
+import Home from '@/assets/icons/home.svg';
+import MapActive from '@/assets/icons/map-active.svg';
+import Map from '@/assets/icons/map.svg';
+import MyActive from '@/assets/icons/my-active.svg';
+import My from '@/assets/icons/my.svg';
+import OrderActive from '@/assets/icons/order-active.svg';
+import Order from '@/assets/icons/order.svg';
 
 import { userNavigations } from '@/constants/navigations';
 import HistoryHomeScreen from '@/screens/history/HistoryHomeScreen';
@@ -18,36 +30,77 @@ export type UserBottomTabsParamList = {
 
 const Tab = createBottomTabNavigator<UserBottomTabsParamList>();
 
+type SvgComponent = (props: SvgProps) => JSX.Element;
+
+const renderTabIcon = (focused: boolean, ActiveIcon: SvgComponent, InactiveIcon: SvgComponent) =>
+  focused ? <ActiveIcon width={28} height={28} /> : <InactiveIcon width={28} height={28} />;
+
 function UserBottomTabsNavigator() {
   return (
     <Tab.Navigator
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        tabBarActiveTintColor: '#0FB758',
+        tabBarInactiveTintColor: '#9C9C9C',
+        headerShown: false,
+        tabBarStyle: {
+          height: 73,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        tabBarItemStyle: {
+          padding: 6, // 아이템 내부 패딩
+          justifyContent: 'center',
+          maxWidth: 76,
+          alignContent: 'center',
+        },
+        tabBarLabelStyle: {
+          marginTop: 2,
+          fontFamily: 'Pretendard-Medium',
+          fontSize: 12,
+        },
+        popToTopOnBlur: true,
+      }}
       initialRouteName={userNavigations.STORE_HOME}
     >
       <Tab.Screen
         name={userNavigations.STORE_HOME}
         component={StoreHomeScreen}
-        options={{ title: '홈' }}
+        options={{
+          title: '홈',
+          tabBarIcon: ({ focused }) => renderTabIcon(focused, HomeActive, Home),
+        }}
       />
       <Tab.Screen
         name={userNavigations.MAP_HOME}
         component={MapHomeScreen}
-        options={{ title: '주변가게' }}
+        options={{
+          title: '주변가게',
+          tabBarIcon: ({ focused }) => renderTabIcon(focused, MapActive, Map),
+        }}
       />
       <Tab.Screen
         name={userNavigations.LIKE_HOME}
         component={LikeHomeScreen}
-        options={{ title: '찜' }}
+        options={{
+          title: '찜',
+          tabBarIcon: ({ focused }) => renderTabIcon(focused, FavoriteActive, Favorite),
+        }}
       />
       <Tab.Screen
         name={userNavigations.HISTORY_HOME}
         component={HistoryHomeScreen}
-        options={{ title: '주문내역' }}
+        options={{
+          title: '주문내역',
+          tabBarIcon: ({ focused }) => renderTabIcon(focused, OrderActive, Order),
+        }}
       />
       <Tab.Screen
         name={userNavigations.MYPAGE_HOME}
         component={MypageHomeScreen}
-        options={{ title: 'MY', headerShown: false }}
+        options={{
+          title: 'MY',
+          tabBarIcon: ({ focused }) => renderTabIcon(focused, MyActive, My),
+        }}
       />
     </Tab.Navigator>
   );

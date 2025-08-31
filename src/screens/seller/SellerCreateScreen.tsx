66d58f470082cd1cd8a1f2a60f537cfc4151f8b4
HeHelee <selly3109@naver.com>
2025-08-31 21:32:56 +0900

    feat: 가게 셍성 페이지 생성 (#53)

---
+++ src/screens/seller/SellerCreateScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SellerCreateScreen = () => {
+  return (
+    <View>
+      <Text>SellerCreateScreen</Text>
+    </View>
+  );
+};
+
+export default SellerCreateScreen;

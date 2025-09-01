3e6e4670bfb4fc3d1a519f977945317ce2d66f4f
최이서 <chtoqur@gmail.com>
2025-09-01 16:26:07 +0900

    fix: 가게 인사이트 훅 수정 (#61)

--- src/hooks/queries/useSeller.ts
+++ src/hooks/queries/useSeller.ts
@@ -2,10 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import useAuth from './useAuth';
 import { getStoreInsight } from '@/api/seller';
 import { queryKeys } from '@/constants/keys';
-import { UseQueryCustomOptions } from '@/types/api';
+import { ResponseError, UseQueryCustomOptions } from '@/types/api';
+import { StoreInsight } from '@/types/domain';
 
-function useGetStoreInsight(queryOptions?: UseQueryCustomOptions) {
-  return useQuery({
+function useGetStoreInsight(queryOptions?: UseQueryCustomOptions<StoreInsight, StoreInsight>) {
+  return useQuery<StoreInsight, ResponseError, StoreInsight>({
     queryFn: getStoreInsight,
     queryKey: [queryKeys.SELLER, queryKeys.GET_STORE_INSIGHT],
     ...queryOptions,

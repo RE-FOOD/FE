afb6171436bd8a773b6074cab16ac70840fb5bb1
최이서 <chtoqur@gmail.com>
2025-09-01 12:16:41 +0900

    feat: 인사이트 api 연동 (#61)

---
+++ src/api/seller.ts
@@ -0,0 +1,11 @@
+import axiosInstance from './axios';
+import { ApiResponse } from '@/types/api';
+import { StoreInsight } from '@/types/domain';
+
+const getStoreInsight = async (): Promise<StoreInsight> => {
+  const { data } = await axiosInstance.get<ApiResponse<StoreInsight>>('/store/mypage');
+  console.log(data);
+  return data.data;
+};
+
+export { getStoreInsight };

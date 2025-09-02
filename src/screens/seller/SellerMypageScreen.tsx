e96231050539f94e7b985af831d75e003ce8e48e
Iseo Choi <chtoqur@gmail.com>
2025-09-03 02:43:13 +0900

    chore: 시연 용 더미데이터 추가

--- src/screens/seller/SellerMypageScreen.tsx
+++ src/screens/seller/SellerMypageScreen.tsx
@@ -17,7 +17,7 @@ const SellerMypageScreen = () => {
 
   const dummyInsight = {
     salesAmount: 125000,
-    popularMenu: ['탕수육', '깐풍기', '김밥'],
+    popularMenu: ['죠죠 야끼소바', '새우구이', '매운 우동'],
     monthAmount: {
       '2025-05': 1050000,
       '2025-06': 800000,

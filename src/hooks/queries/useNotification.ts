05dc4042395a007ebb1679f95e37d28adc30ca4c
최이서 <chtoqur@gmail.com>
2025-09-02 20:34:19 +0900

    feat: 알림함 무한 스크롤 훅 추가 (#67)

--- src/hooks/queries/useNotification.ts
+++ src/hooks/queries/useNotification.ts
@@ -3,19 +3,17 @@ import { getNotificationList, NotiResponse } from '@/api/notification';
 import { queryKeys } from '@/constants/keys';
 
 export const useNotification = () => {
-  return useInfiniteQuery<
-    NotiResponse, // queryFnData (API 응답 1페이지)
-    Error, // error 타입
-    InfiniteData<NotiResponse>, // data 타입 👈 여기!
-    [string], // queryKey
-    number // pageParam
-  >({
+  return useInfiniteQuery<NotiResponse, Error, InfiniteData<NotiResponse>, [string], number>({
     queryKey: [queryKeys.NOTIFICATION],
     queryFn: () => getNotificationList(),
     initialPageParam: 0,
-    getNextPageParam: (lastPage) => {
+    getNextPageParam: (lastPage, allPages) => {
       if (lastPage.nextCursor == null) return undefined;
-      if (lastPage.list.length === 0) return undefined;
+      if (lastPage.list.length < 15) return undefined;
+
+      const prevPage = allPages[allPages.length - 2];
+      if (prevPage && prevPage.nextCursor === lastPage.nextCursor) return undefined;
+
       return lastPage.nextCursor;
     },
   });

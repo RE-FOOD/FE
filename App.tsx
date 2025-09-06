8ec22fd5f2b6f8f51602398779ae69a5d2efe056
Iseo Choi <chtoqur@gmail.com>
2025-09-07 01:14:22 +0900

    fix: 스플래시 스크린 중복 현상 해결 (#77)

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from '@/api/queryClient';
 import { colors } from '@/constants/colors';
+import useAuth from '@/hooks/queries/useAuth';
 import { NotificationProvider } from '@/hooks/useNotification';
 import RootNavigator from '@/navigations/root/RootNavigator';
 import pushNoti from '@/utils/pushNoti';
@@ -55,7 +56,15 @@ const toastConfig = {
   ),
 };
 
-function AppContent() {
+function AppContentInner() {
+  const { isLoading } = useAuth();
+
+  useEffect(() => {
+    if (!isLoading) {
+      BootSplash.hide({ fade: true });
+    }
+  }, [isLoading]);
+
   useEffect(() => {
     async function setupNotificationChannel() {
       try {
@@ -66,7 +75,6 @@ function AppContent() {
         });
         console.log('Notification channel created:', channel);
 
-        // 채널 존재 확인
         const channels = await notifee.getChannels();
         console.log('Available channels:', channels);
       } catch (error) {
@@ -90,16 +98,17 @@ function AppContent() {
   }, []);
 
   return (
+    <NavigationContainer theme={AppTheme}>
+      <RootNavigator />
+      <Toast config={toastConfig} />
+    </NavigationContainer>
+  );
+}
+
+function AppContent() {
+  return (
     <QueryClientProvider client={queryClient}>
-      <NavigationContainer
-        theme={AppTheme}
-        onReady={() => {
-          BootSplash.hide({ fade: true });
-        }}
-      >
-        <RootNavigator />
-        <Toast config={toastConfig} />
-      </NavigationContainer>
+      <AppContentInner />
     </QueryClientProvider>
   );
 }
@@ -111,4 +120,5 @@ function App() {
     </NotificationProvider>
   );
 }
+
 export default App;

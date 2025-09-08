26385eacfae4bf124a21c66ac469c58c78f4b86e
HeHelee <selly3109@naver.com>
2025-09-08 18:12:02 +0900

    feat: 버튼 스타일링 변경 (#83)

--- src/screens/seller/SellerMenuRegisterScreen.tsx
+++ src/screens/seller/SellerMenuRegisterScreen.tsx
@@ -371,13 +371,15 @@ const styles = StyleSheet.create({
     marginLeft: 25 + 80 + 5,
     paddingVertical: 12,
     borderRadius: 6,
-    backgroundColor: colors.GRAY_200,
+    backgroundColor: colors.WHITE,
+    borderColor: colors.GREEN,
+    borderWidth: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   imageButtonText: {
     fontSize: 16,
-    color: colors.BLACK,
+    color: colors.GREEN,
     fontFamily: 'Pretendard-Regular',
   },
   image: {
